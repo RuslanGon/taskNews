74e0b0a50d31de756064abc2b30d80edfc7f23b6
RuslanGon <ruslan441006@gmail.com>
2025-12-16 20:41:51 +0200

    wer

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,14 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_NEWS_API_TOKEN: string;
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
-  
+  readonly VITE_NEWS_API_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
